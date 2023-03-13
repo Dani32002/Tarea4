c7cd5ed8e63a9955e492554901e750508ba979c6
Dani <d.escalante@uniandes.edu.co>
2023-03-13 18:09:09 -0500

    Pregunta1 main

--- Tarea 4/src/Pregunta1/Pregunta1.java
+++ Tarea 4/src/Pregunta1/Pregunta1.java
@@ -1,6 +1,7 @@
 package Pregunta1;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import modulo.Carga;
 
@@ -10,11 +11,25 @@ public class Pregunta1 {
 		String algoritmo = Carga.class.getPackage().getName() + "." + args[0];
 		@SuppressWarnings("deprecation")
 		AlgoritmoGrafoCostoMin calculadora = (AlgoritmoGrafoCostoMin) Class.forName(algoritmo).newInstance();
-		carga.cargarMatriz(args[1]);
+		ArrayList<ArrayList<Integer>> matriz = carga.cargarMatriz(args[1]);
 		
 		long inicioTiempo = System.currentTimeMillis();
-		
+		ArrayList<ArrayList<Integer>> respuesta = calculadora.algoritmo(matriz);
 		long finalTiempo = System.currentTimeMillis();
+		
+		System.out.print("{");
+		for (ArrayList<Integer> fila: respuesta) {
+			System.out.print("{");
+			for (int i = 0; i < fila.size(); i++) {
+				if (i == fila.size() - 1) {
+					System.out.print(fila.get(i));
+				} else {
+					System.out.print(fila.get(i) + ",");
+				}
+			}
+			System.out.print("}\n");
+		}
+		System.out.print("}\n");
 		System.out.println("Tiempo Total (milisegundos): "+(finalTiempo-inicioTiempo));
 	}
 }

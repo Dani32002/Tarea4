4a1623fd6f32c1b6db7a3e110fa7103f6ad5d1f2
Samuel Goncalves <s.goncalves@uniandes.edu.co>
2023-03-15 16:39:12 -0500

    Implementacion dijkstra

--- src/Pregunta1/Dijkstra.java
+++ src/Pregunta1/Dijkstra.java
@@ -1,21 +1,54 @@
 package Pregunta1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Dijkstra implements AlgoritmoGrafoCostoMin{
 
 	@Override
 	public ArrayList<ArrayList<Integer>> algoritmo(ArrayList<ArrayList<Integer>> matriz) {
-		
-		
-		return null;
+		ArrayList<ArrayList<Integer>> D = new ArrayList<ArrayList<Integer>>();
+		for (int k = 0; k<matriz.size(); k++) {
+			ArrayList<Integer> C = DijkstraImplementacion(matriz, k);
+			D.add(C);
+		}
+		return D;
 	}
 	
 	
-	public ArrayList<Integer> Dijkstra(ArrayList<ArrayList<Integer>> matriz, int inicio) {
+	public ArrayList<Integer> DijkstraImplementacion(ArrayList<ArrayList<Integer>> matriz, int inicio) {
+		ArrayList<Integer> R = new ArrayList<Integer>();
+		ArrayList<Integer> C = new ArrayList<Integer>();
+		ArrayList<Integer> P = new ArrayList<Integer>();
 		
-		return null;
-		
+		R.add(inicio);
+		for (int i = 0; i<matriz.get(inicio).size(); i++) {
+			C.add(i, matriz.get(inicio).get(i));
+			P.add(i);
+			
+		}
+		while (!R.equals(P)){
+			int w = inicio;
+			for (int j = 0; j<matriz.get(inicio).size(); j++) {
+				if (!R.contains(j) && (w == inicio || C.get(j)<C.get(w))){
+					w = j;
+				}
+			}
+			R.add(w);
+			Collections.sort(R);
+			for(int s = 0; s<matriz.get(inicio).size(); s++) {
+				if(!R.contains(s)) {
+					if (matriz.get(w).get(s) == 2147483647) {
+						C.set(s, C.get(s));
+					}
+					else {
+						C.set(s, Math.min(C.get(s),C.get(w)+ matriz.get(w).get(s)));
+					}
+					
+				}
+			}
+		}
+		return C;
 	}
 
 	
